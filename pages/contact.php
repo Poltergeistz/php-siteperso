2e3a53f2dd1981e476fbd4e92c29e1c15ee2364d
david <david.journo.31@gmail.com>
2018-07-29 16:02:54 +0200

    Partie 5 : formulaire & recupération des messages

--- pages/contact.php
+++ pages/contact.php
@@ -1,1 +1,14 @@
-<p>contact</p>
+<h1>contact</h2>
+
+<h4> Votre message </h4>
+<form action="/save.php" method="POST">
+<div>
+<label>Email</label>
+<input type="email" name="email" id="email">
+</div>
+<div>
+<label>Message</label>
+<textarea rows="6" name="message"></textarea>
+<input type="submit" name="envoyer">
+</div>
+</form>
