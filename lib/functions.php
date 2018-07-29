e463567eaf383f1b373553c219460cddb81efef7
david <david.journo.31@gmail.com>
2018-07-29 15:27:37 +0200

    Partie 4 : function getUserData()

--- lib/functions.php
+++ lib/functions.php
@@ -10,14 +10,31 @@ function getContent(){
 	if(!isset($_GET['page'])){
 		include __DIR__.'/../pages/home.php';
 	} 
-	else if($_GET['page'] == "bio"){
+	else if($_GET['page'] == 'bio'){
 		// le reste du code
 		include __DIR__.'/../pages/bio.php';
-	} else if($_GET['page'] == "contact"){
+	} else if($_GET['page'] == 'contact'){
 		include __DIR__.'/../pages/contact.php';
 	}
 }
 
 function getPart($name){
 	include __DIR__ . '/../parts/'. $name . '.php';
+}
+
+function getUserData(){
+	$data = file_get_contents(__DIR__.'/../data/user.json');
+	$decode = json_decode($data);
+	foreach($decode as $key => $value){
+		echo("<br>"."<b>".$key." :</b> $value "); // Affiche l'array a cause du foreach 
+		$type = gettype($value);
+            if ($type == "array") {
+				foreach($value as $val) {
+					echo("<br>");
+					echo("<br>".$val->year);
+					echo("<br>".$val->company);
+					echo("<br>");
+				}
+			}
+		}
 }
