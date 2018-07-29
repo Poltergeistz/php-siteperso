d1b996a0936d4327341d36b252fc99e8d2ba2fef
david <david.journo.31@gmail.com>
2018-07-29 18:44:48 +0200

    Ajout de contenu

--- pages/home.php
+++ pages/home.php
@@ -8,36 +8,53 @@
 <div class="ui three column grid link cards">
 <div class="ui card">
 <div class="image">
-  <img src="./img/Chat_Pose.jpeg">
+  <img src="https://i.imgur.com/IkG8mdb.png">
   </div>
   <div class="content">
-  <a href="" class="header">Game Jam : Coop'Café</a>
+  <a href="https://github.com/Poltergeistz/gameJam-maisonAvenir" class="header">Game Jam : Coop'Café</a>
   <div class="meta">
     <span class="date"></span>
     </div>
     <div class="description">
-<p>lorem ipsum</p>
+<p>Favoriser les échanges entre les talentueux membres de la communauté Repar'café.</p>
 </div>
 </div>
 <div class="extra content">
-		<span class="right floated">Début : </span>
+		<span class="right floated">Date : 25 - 27 Mai 2018</span>
 </div>
 </div>
 <div class="ui card">
 <div class="image">
-  <img src="./img/Chat_Vacances.jpeg">
+  <img src="img/Chat_Vacances.jpeg">
   </div>
   <div class="content">
-  <a href="" class="header">Projet Pédagogique : Ecokid</a>
+  <a href="https://github.com/Djohn12/Glossaire" class="header">Projet fil rouge : Glossaire</a>
   <div class="meta">
     <span class="date"></span>
     </div>
     <div class="description">
-    <p>lorem ipsum</p>
+<p>Réaliser un glossaire pour la promotion 2 de Simplon.</p>
+</div>
+</div>
+<div class="extra content">
+		<span class="right floated">Date : Tout le long de la formation</span>
+</div>
+</div>
+<div class="ui card">
+<div class="image">
+  <img src="https://i.imgur.com/JkdCcXF.png">
+  </div>
+  <div class="content">
+  <a href="https://github.com/Poltergeistz/Projet-Pedagogique-Info-Energie" class="header">Projet Pédagogique : Ecokid</a>
+  <div class="meta">
+    <span class="date"></span>
+    </div>
+    <div class="description">
+    <p>Battre son record d'éco-gestes dans la maison pour rompre la maléfice d'Energeox !</p>
 	</div>
     </div>
     <div class="extra content">
-		<span class="right floated">Début : </span>
+		<span class="right floated">Date : 9 - 22 Juillet 2018</span>
 	</div>
 </div>
 </div>
